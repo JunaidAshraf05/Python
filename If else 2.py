a6d38d10a817ac09edbbb8bddafb71ec1839bb29
sStudent <junaidashraf2500@gmail.com>
2024-09-24 19:26:48 +0530

    Python

---
+++ If else 2.py
@@ -0,0 +1,8 @@
+ #another example
+
+applePrice = 210
+budget = 200
+if(applePrice<=budget):
+    print("Alexa , add 1 kg Apple to the cart .")
+else:
+    print("Alexa , do not add Apple to the cart.")
