91437d6991f745ef702ca946cac6b0ce8a2d31b1
sStudent <junaidashraf2500@gmail.com>
2024-09-26 17:50:17 +0530

    python

---
+++ Do while.py
@@ -0,0 +1,11 @@
+#do while loop me ekk na ekk barr tera code chlaega agr tera condition  sahi ho ya na ho aur uske agli iteration tabhi chalegi agr tera conditon  sahi hai
+
+
+i = 0
+while True:
+  print(i)
+  i = i+1 
+  if(i%100 == 0):
+      break    
+
+
