0c9a6a1dfd44b2536840043d41938fa2de8913fc
sStudent <junaidashraf2500@gmail.com>
2024-09-26 19:36:30 +0530

    python

--- Functions .py
+++ Functions .py
@@ -1,5 +1,23 @@
 #code for geometric mean                               
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+
+
 a = 9 
 b = 8
-gmean1 = (a*b)/(a+b)
-print(gmean1)
+if(a>b):
+    print("First number is greater")
+else:
+    print("second number is greater or equal")
+calculateGmean( a ,b )
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+
+c = 8
+d = 74
+if(c>d):
+    print("First number is greater")
+else:
+    print("second number is greater or equal")
