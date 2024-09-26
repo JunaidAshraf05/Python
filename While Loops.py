8d9dba38f13b62d16818e905da1bfbf3dfc44ab7
sStudent <junaidashraf2500@gmail.com>
2024-09-26 16:32:00 +0530

    Phython

---
+++ While Loops.py
@@ -0,0 +1,13 @@
+#it will print till 2 because i is less than 3
+i = 0
+while(i<3):
+ print(i)
+ i = i + 1
+  
+#it will print till 3 because is less than and equal to 3
+i = 0 
+while(i<=3):
+ print(i)
+ i = i + 1
+ 
+print("Done with the loop")
