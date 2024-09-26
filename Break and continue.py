8b81bc37b567552b05894f82d16f8e644467d330
sStudent <junaidashraf2500@gmail.com>
2024-09-26 17:44:30 +0530

    python

---
+++ Break and continue.py
@@ -0,0 +1,26 @@
+#multiplication of any nuumber
+
+#break matlaab loop ko chodkar nikal jaoo
+
+#continue matlab iteration ko chodkar nikl jao
+
+
+# for i in range(10):
+   
+#          print("5 X" , i+1 , '=' , 5 * ( i+1) )
+#          if( i == 10):
+#              break
+         
+         
+         
+for i in range(10):
+    if( i == 10):
+        print("skip the iteration")
+        continue
+   
+print("5 X" , i+1 , '=' , 5 * ( i+1) )
+         
+            
+         
+    
+    
