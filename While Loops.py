bc539aef8553a9f91a460424c7282a83f0ef73c7
sStudent <junaidashraf2500@gmail.com>
2024-09-26 17:28:42 +0530

    Python

--- While Loops.py
+++ While Loops.py
@@ -10,4 +10,26 @@ while(i<=3):
  print(i)
  i = i + 1
  
-print("Done with the loop")
+
+
+#it will take the user input till the condition is not satisfied
+# i = int(input("Enter the number: "))
+# while(i<=40):
+#    i =int(input("Enter the number: "))
+#    print(i)
+   
+# print("done")
+
+
+
+#Decrementing While loop
+cout = 5
+while (cout > 0):
+    print(cout)
+    cout = cout - 1
+ 
+else:
+    print("I am inside else")    
+
+  
+  
