18d35f5f07cf90c4a20a870b9144660f74865409
sStudent <junaidashraf2500@gmail.com>
2024-09-26 16:24:04 +0530

    Python

--- For Loops .py
+++ For Loops .py
@@ -1,15 +1,22 @@
-#Example:iteration over a string
-name = "Junaid"
-for i in name:
-    print(i)
-    if( i =="u"):
-         print("This is special")
+# name = "Junaid"
+# for i in name:
+#     print(i)
+#     if( i =="u"):
+#          print("This is special")
          
          
  #Example:iteration over a list:
  
-colors = ["RED" , "GREEN" , "Blue" , "Yellow"]       
-for color in colors:
-    print(color) 
-    for i in color:
-        print(i) 
+# colors = ["RED" , "GREEN" , "Blue" , "Yellow"]       
+# for color in colors:
+#     print(color) 
+#     for i in color:
+#         print(i) 
+
+
+# for k in range(5):
+#  print(k)
+ 
+ 
+for k in range(1 , 12 , 3):
+     print(k)
