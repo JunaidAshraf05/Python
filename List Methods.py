e94e5a73d59d3072988c79f758dea0bbd21d38af
sStudent <junaidashraf2500@gmail.com>
2024-09-28 15:21:38 +0530

    python

---
+++ List Methods.py
@@ -0,0 +1,20 @@
+l = [11,34,69,45,324,2,3,4,5,1]
+
+l.append(7) #it will print 7 in the end 
+print(l)
+l.sort()#it arrange in ascending 
+print(l)
+l.sort(reverse=True)#it will arrange in descending order
+print(l)
+l.reverse()#it will reverse the list
+print(l)
+
+print(l.count(1))#it will count the defined number in given statement
+print(l)
+
+l.insert(1,899)
+print(l)# it will the number in index
+
+m = [989 , 1000 , 11000]
+l.extend(m)
+print(l) #it will extend the given index put these defined number
--- Lists .py
+++ Lists .py
@@ -3,7 +3,7 @@ print(marks)
 print(type(marks))
 
  
-marks = [ 3 ,35 ,6,"Junaid","True"]
+marks = [ 3 ,35 ,6,"Junaid","True" , 8 , 9 , 5, 7 ,9 ,2 ,1 ,4 ]
 print(marks)
 print(type(marks))
 #list can contains another datatype too
@@ -15,8 +15,22 @@ print(marks[4])
 print(marks[5])
  
  
-print(marks[-3])#negative index 
-print(marks(len(marks)-3))# positive index
+print(marks[-3]) #negative index 
+# print(marks(len(marks)-3)) # positive index
  
 print(marks[5-3])#positive index
-print(marks[2])#positice inex
+print(marks[2])#positive inex
+
+if "junaid" in marks:
+  print("yes")
+else:
+    print("no")
+    
+
+if "unaid" in "junaid":
+    print("yes")
+    
+
+print(marks)
+print(marks[1:-1])
+print(marks[1:4:2])
