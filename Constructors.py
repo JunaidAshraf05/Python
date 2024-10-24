746edecce1b67289fcfbb727b583a5c6051c5bf8
sStudent <junaidashraf2500@gmail.com>
2024-10-24 19:40:37 +0530

    Python

---
+++ Constructors.py
@@ -0,0 +1,12 @@
+class person:
+
+ def _ _init_ _(self)
+ 
+def info(self):
+  print(f"Name: {self.name} is a {self.occ}" )
+  
+  a =person()
+#   print(a.Name)
+#   a.name="divya"
+#   a.occ="hr"
+#   a.info()    A    
