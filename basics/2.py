2b5ab2a46b66ca51eca30b47ced1847f8b0b35ad
sStudent <junaidashraf2500@gmail.com>
2024-11-17 13:14:39 +0530

    Python

---
+++ basics/2.py
@@ -0,0 +1,22 @@
+#mutable and immutable
+#mutable: list, dict, set
+#immutable: int, float, str, tuple, bool, NoneType
+#list
+my_list = [1, 2, 3, 4, 5]
+print(my_list[0])  # prints: 1
+#list methods
+my_list.append(6)
+print(my_list)  # prints: [1, 2, 3, 4,
+#dict
+my_dict = {"name": "John", "age": 30}
+print(my_dict["name"])  # prints: John
+#dict methods
+my_dict["city"] = "New York"
+print(my_dict)  # prints: {'name': 'John', 'age': 30,
+#set
+my_set = {1, 2, 3, 4, 5}
+print(my_set)  # prints: {1, 2, 3, 4,
+#set methods
+my_set.add(6)
+print(my_set)  # prints: {1, 2, 3, 4,
+
