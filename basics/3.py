96660f3a22e307eae6315dfb553674f434d5a9c0
sStudent <junaidashraf2500@gmail.com>
2024-11-19 11:26:10 +0530

    Python

--- basics/3.py
+++ basics/3.py
@@ -1,1 +1,55 @@
+2**88
+309485009821345068724781056
+import math
+math.pi
+3.141592653589793
 
+# it will generate a random number
+import random.random()
+
+
+random.choice([1,2,3,4,5,6,7,8,9]) 
+7
+ 
+random.choice([1,2,3,4,5,6,7,8,9])
+5
+random.choice([1,2,3,4,5,6,7,8,9])
+5
+
+random.choice([1,2,3,4,5,6,7,8,9])
+3
+
+username ="junaid"
+len(username)
+6
+username[0]
+'j'
+
+username[-1]
+'d'
+username[-2]
+'i'
+username[1:2]
+'u'
+
+
+dir(username)
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'i'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 
+'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+
+mylist=[123,"junaid",3.14]
+mylist
+[123, 'junaid', 3.14]
+len(mylist)
+3
+mylist[0]
+123
+myD ={'one':'lemon','two':'ginger','comic':'naagraj'}    
+myD
+{'one': 'lemon', 'two': 'ginger', 'comic': 'naagraj'}        
+myD['one']
+'lemon'
+
+myTup = (1,2,3)
+myTup[0]
+1
