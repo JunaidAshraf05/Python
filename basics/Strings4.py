e485c1535df62c2b7ba467c1cab937af030f5e19
sStudent <junaidashraf2500@gmail.com>
2024-11-20 11:58:07 +0530

    Pyhton

--- basics/Strings4.py
+++ basics/Strings4.py
@@ -7,6 +7,18 @@ first_char = chai[0]
 print(first_char)
 # first_char = "m"
 
+print(chai.lower())
+# masala chai
+print(chai.upper())
+# MASALA CHAI
+
+chai = "   masala chai   "
+chai
+'   masala chai   '
+print(chai.strip())
+# masala chai
+
+
 num_list = "0123456789"
 num_list[:]
 '0123456789'
@@ -18,3 +30,8 @@ num_list[3]
 '3456789'
 num_list[:7]
 '01234567'
+
+num_list[0:5:7]
+
+
+
