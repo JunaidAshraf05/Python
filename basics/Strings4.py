2477980c91f2915468a837e6e9aeb01b4d1edbd5
sStudent <junaidashraf2500@gmail.com>
2024-11-20 11:36:23 +0530

    Python

--- basics/Strings4.py
+++ basics/Strings4.py
@@ -7,4 +7,14 @@ first_char = chai[0]
 print(first_char)
 # first_char = "m"
 
-
+num_list = "0123456789"
+num_list[:]
+'0123456789'
+num_list[:3]
+'012'
+num_list[3:]
+'3456789'
+num_list[3]
+'3456789'
+num_list[:7]
+'01234567'
