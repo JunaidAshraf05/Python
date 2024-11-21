dcf1954b7693e502a709298878e9106cee340863
sStudent <junaidashraf2500@gmail.com>
2024-11-21 19:27:01 +0530

    Python

---
+++ basics/list5.py
@@ -0,0 +1,9 @@
+tea_varieties = ["black" , "green" , "oolong" , "white"]
+print(tea_varieties)
+
+print(tea_varieties[-1])
+# white
+print(tea_varieties[1:3])
+# ['green', 'oolong']
+print(tea_varieties[:2])
+['black', 'green']
