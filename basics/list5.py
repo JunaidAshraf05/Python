a78fb94f7b4bf6da24a22c9561f647071125d7b3
sStudent <junaidashraf2500@gmail.com>
2024-11-21 19:32:41 +0530

    Python

--- basics/list5.py
+++ basics/list5.py
@@ -5,5 +5,22 @@ print(tea_varieties[-1])
 # white
 print(tea_varieties[1:3])
 # ['green', 'oolong']
+
 print(tea_varieties[:2])
 ['black', 'green']
+
+print(tea_varieties[2:])
+['oolong', 'white']
+
+print(tea_varieties[3])
+white
+
+tea_varieties[3]="herbal"
+print(tea_varieties)
+['black', 'green', 'oolong', 'herbal']
+
+tea_varieties[1:2]
+['green']
+tea_varieties[1:2]="lemon"
+tea_varieties
+['black', 'l', 'e', 'm', 'o', 'n', 'oolong', 'herbal']
